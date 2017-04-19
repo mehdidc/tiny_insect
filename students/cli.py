9bd9cc9f77d0fd6be29af304b6b2127a5985d168
mehdi cherti <mehdi@cherti.name>
2017-04-19 12:36:16 +0200

    update

--- students/cli.py
+++ students/cli.py
@@ -45,6 +45,7 @@ from data import norm
 from data import Tiny
 from data import MergeLoader
 from data import DataAugmentationLoaders
+from data import GeneratorLoaderDomainTransfer
 
 sys.path.append('../generators')
 
@@ -100,6 +101,33 @@ class Gen(nn.Module):
     def forward(self, input):
         x = self.main(input)
         return x
+
+
+class GenDt(nn.Module):
+    def __init__(self, nz=100, nc=3, nb_classes=10):
+        super(GenDt, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(nz + nc + nb_classes, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.ConvTranspose2d(64 * 4, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64 * 2,     64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(    64,      nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        return self.main(input)
 
 
 class MLPStudent(nn.Module):
@@ -169,6 +197,33 @@ class Conv2FcStudent(nn.Module):
         return x
 
 
+class Conv2FullyStudent(nn.Module):
+    def __init__(self, nc, w, h, no, nbf1=512, nbf2=512, nbf3=512, sf=5):
+        super(Conv2FullyStudent, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(nc, nbf1, sf),
+            nn.ReLU(True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(nbf1, nbf2, sf),
+            nn.ReLU(True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(nbf2, nbf3, sf),
+            nn.ReLU(True),
+
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(nbf3, no)
+        )
+        
+    def forward(self, input):
+        x = self.features(input)
+        x = nn.AvgPool2d(kernel_size=x.size()[2:])(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
 def weights_init(m, xavier=False):
     classname = m.__class__.__name__
     if classname.find('Conv2d') != -1:
@@ -190,11 +245,20 @@ def get_acc(pred, true):
     return (pred_classes == true_classes).float().mean()
 
 
-def insert(*, nb=1, data_source=None, model=None, bayesopt=False):
+def insert(*, nb=1, data_source=None, model=None, bayesopt=False, hypers_from=None):
     nb_inserted = 0
     db = load_db()
     params_list = _sample(nb=nb, data_source=data_source, model=model, bayesopt=bayesopt)
+
+    if hypers_from:
+        job = db.get_job_by_summary(hypers_from)
+        hypers = job['content']['hypers']
+    else:
+        hypers = None
+
     for params in params_list:
+        if hypers:
+            params['hypers'] = hypers
         print(params)
         _check(params)
         nb_inserted += db.safe_add_job(params, model=params['model'])
@@ -241,9 +305,21 @@ def _sample(nb=1, data_source=None, model=None, bayesopt=False):
 
 def _sample_unif(rng, model=None, data_source=None):
     if not model:
-        model = rng.choice(('convfc', 'conv2fc'))
+        model = rng.choice(('convfc', 'conv2fc', 'conv2fully'))
     if not data_source:
-        data_source = rng.choice(('dataset', 'aux2', 'dataset_simple', 'aux3'))
+        data_source = rng.choice((
+            'dataset', 
+            'dataset_simple', 
+            'dataset_raw',
+            'tiny',
+            'aux2', 
+            'aux3', 
+            'dataset_simple,aux2', 
+            'dataset_simple,aux3', 
+            'dataset_simple,dataset_simple',
+            'aux2_big',
+            'aux3_big'
+        ))
     if model == 'convfc':
         sf = rng.choice((3, 5))
         nbf = rng.choice((32, 64, 96, 128, 192, 256, 512, 600, 650, 700, 800, 900, 1000))
@@ -265,6 +341,17 @@ def _sample_unif(rng, model=None, data_source=None):
             'nbf2': nbf2,
             'sf': sf,
             'fc': fc,
+        }
+    elif model == 'conv2fully':
+        sf = rng.choice((3, 5))
+        nbf1 = rng.choice((32, 64, 96, 128, 192, 256, 512))
+        nbf2 = rng.choice((32, 64, 96, 128, 192, 256, 512))
+        nbf3 = rng.choice((32, 64, 96, 128, 192, 256, 512))
+        hypers = {
+            'nbf1': nbf1,
+            'nbf2': nbf2,
+            'nbf3': nbf3,
+            'sf': sf,
         }
     elif model == 'mlp':
         fc1 = rng.randint(1, 10) * 100
@@ -303,53 +390,71 @@ def _sample_bayesopt(*, nb=1, sample_func=_sample_unif, data_source=None):
     from lightjob.db import SUCCESS
     from sklearn.ensemble import RandomForestRegressor
     import pandas as pd
+    db = load_db()
+    jobs = list(db.jobs_with_state(SUCCESS)) + list(db.jobs_with_state(ERROR))
     reg = RandomForestRegressorWithUncertainty(
         n_estimators=100, 
         min_samples_leaf=5,
         oob_score=True)
+    extra = [j['content'] for j in jobs]
     opt = Bandit(
         sampler=sample_func, 
         score=ucb_maximize, 
-        model=Wrapper(reg, transform_X=_transform),
+        model=Wrapper(reg, transform_X=partial(_transform, extra=extra)),
         nb_suggestions=1000
     )
-    db = load_db()
-    jobs = db.jobs_with_state(SUCCESS)
-    jobs = list(jobs)
     if data_source:
         jobs = [j for j in jobs if j['content']['data_source'] == data_source]
     X = [j['content'] for j in jobs]
-    y = [np.max(j['stats']['valid_acc']) for j in jobs]
+    y = [np.max(j['stats']['valid_acc']) if j['state'] == SUCCESS else 0 for j in jobs]
     print('{} examples from the surrogate to learn from'.format(len(X)))
     opt.update_many(X, y)
+    print(reg.oob_score_)
     return [opt.suggest() for _ in range(nb)]
 
 
-def _transform(dlist):
+def _transform(dlist, extra=None):
     import copy
     from fluentopt.transformers import vectorize
+    from fluentopt.utils import flatten_dict
+    rng = np.random
+
+
+    extra = copy.deepcopy(extra)
+    extra.extend([_sample_unif(np.random) for _ in range(1000)])
     dlist = copy.deepcopy(dlist)
-    for d in dlist:
-        d['algo'] = {'adam': 0, 'nesterov': 1, 'sgd': 2}[d['algo']]
-        d['data_source'] = {'aux1' : 0, 'aux2': 1, '' 'dataset': 2, 'dataset_old': 3, 'dataset_simple': 4, 'aux3': 5, 'tiny': 6}[d['data_source']]
-        d['momentum'] = d['momentum'] if d['momentum'] else -1
-        d['model'] = {'convfc': 0, 'mlp': 1, 'conv2fc': 2}[d['model']]
-        d['xavier'] = d.get('xavier', 0)
-    v  = vectorize(dlist)
-    v[np.isnan(v)] = -1
-    return v
+    dlist.extend(extra)
+    for i, d in enumerate(dlist):
+        dlist[i] = flatten_dict(d)
+    df = pd.DataFrame(dlist)
+    df = pd.get_dummies(df)
+    df = df.fillna(-1)
+    df = df[0:-len(extra)]
+    return df.values
 
 
 def _check(params):
-    allowed = ('aux1', 'aux2', 'dataset', 'dataset_simple', 'aux3', 'tiny', 'dataset_raw')
+    allowed = (
+         'aux1', 
+        'aux2', 
+        'dataset', 
+        'dataset_simple', 
+        'aux3', 
+        'tiny', 
+        'dataset_raw', 
+        'cond1',
+        'tiny_dt'
+    )
     data_source = params['data_source']
     if ',' in data_source:
         for ds in data_source.split(','):
             assert ds in allowed, 'Wrong data source : "{}"'.format(ds)
         return
     else:
+        if '_big' in data_source:
+            data_source = data_source.replace('_big', '')
         assert data_source in allowed, 'Wrong data source : "{}"'.format(data_source)
-
+    
 
 def clean():
     from shutil import rmtree
@@ -446,6 +551,23 @@ def train_random(*, data_source=None, model=None, bayesopt=False):
     _train_model(params)
 
 
+def resume(*, job, folder='out/tmp', budget_secs=3600., filename=None, lr=None):
+    db = load_db()
+    job = db.get_job_by_summary(job)
+    params = job['content']
+    params['folder'] = folder
+    params['budget_secs'] = budget_secs
+    if filename:
+        params['student'] = filename
+    else:
+        params['student'] = 'jobs/{}/student.th'.format(job['summary'])
+    if lr:
+        lr = float(lr)
+        print('previous lr : {}, new lr : {}'.format(params['lr'], lr))
+        params['lr'] = lr
+    _train_model(params)
+
+
 def _get_data(data_source, batchSize=32, augment=True):
     np.random.seed(42)
     dataroot = '/home/mcherti/work/data/cifar10'
@@ -461,6 +583,7 @@ def _get_data(data_source, batchSize=32, augment=True):
         'aux2' : '../generators/samples/samples_pretrained_aux_cifar/netG_epoch_72.pth',     #trained using pretrained_aux_dcgan_32.py
         'aux3' : '../generators/samples/samples_cond_dcgan_cls_32/netG_epoch_72.pth',        #trained using cond_dcgan_cls_32.py
         'cond1': '../generators/samples/samples_cond_dcgan_cifar10/netG_epoch_329.pth',      #trained using cond_dcgan_32.py
+        'tiny_dt': '../generators/samples/samples_cond_dcgan_dtcls_2_32/netG_epoch_10.pth'
     }
     classifier = '/home/mcherti/work/code/external/densenet.pytorch/model/model.th'
     if ',' in data_source:
@@ -556,6 +679,46 @@ def _get_data(data_source, batchSize=32, augment=True):
         dataloader_train = torch.utils.data.DataLoader(
             tiny, batch_size=batchSize,
             num_workers=1)
+        dataloader_valid = torch.utils.data.DataLoader(
+            dataset_valid, batch_size=batchSize,
+            sampler=SamplerFromIndices(dataset, perm_valid),
+            num_workers=8)
+
+    elif data_source_prefix == 'tiny_dt':
+        predictions_filename = 'predictions/{}.th'.format(data_source)
+        perm = np.arange(len(dataset))
+        np.random.shuffle(perm)
+        perm = torch.from_numpy(perm)
+        perm_train = perm[0:40000]
+        perm_valid = perm[40000:]
+        nb_train_examples = len(perm_train)
+        nb_valid_examples = len(perm_valid)
+        tiny = Tiny(
+           '/home/mcherti/work/data/tiny_images/tiny_images_subset.bin', 
+           transform=transforms.Compose([
+               transforms.ToTensor(),
+               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        )
+        source = torch.utils.data.DataLoader(
+            tiny, batch_size=batchSize,
+            num_workers=1)
+        source = DataAugmentationLoader(source, nb_epochs=1)
+
+        G = GenDt(nz=nz, nc=3, nb_classes=nb_classes)
+        G.load_state_dict(torch.load(generator))
+        G = G.cuda()
+
+        z = torch.randn(batchSize, nz, 1, 1)
+        z = Variable(z)
+        z = z.cuda()
+
+        onehot = torch.zeros(batchSize, nb_classes, 1, 1)
+        onehot = Variable(onehot)
+        onehot = onehot.cuda()
+
+        u = torch.zeros(batchSize, nb_classes, 1, 1)
+        u = u.cuda()
+        dataloader_train = GeneratorLoaderDomainTransfer(G=G, z=z, onehot=onehot, u=u, source=source) 
         dataloader_valid = torch.utils.data.DataLoader(
             dataset_valid, batch_size=batchSize,
             sampler=SamplerFromIndices(dataset, perm_valid),
@@ -709,11 +872,22 @@ def _train_model(params):
         fc1 = hypers['fc1']
         fc2 = hypers['fc2']
         S = MLPStudent(3, imageSize, imageSize, nb_classes, fc1=fc1, fc2=fc2)
+    elif m == 'conv2fully':
+        nbf1 = hypers['nbf1']
+        nbf2 = hypers['nbf2']
+        nbf3 = hypers['nbf3']
+        S = Conv2FullyStudent(3, imageSize, imageSize, nb_classes, nbf1=nbf1, nbf2=nbf2, nbf3=nbf3)
+    else:
+        raise ValueError('Wrong model : {}'.format(m))
     
+    print('Nb of params : {}'.format(_get_nb_params(S)))
+
     if params.get('xavier') == True:
         S.apply(partial(weights_init, xavier=True))
     else:
         S.apply(weights_init)
+    if 'student' in params:
+        S = torch.load(params['student'])
     S = S.cuda()
     
     input = torch.zeros(batchSize, 3, imageSize, imageSize)
@@ -829,5 +1003,97 @@ def _train_model(params):
     }
     return result
 
+def export(filename='jobs.json'):
+    df = _build_jobs_df()
+    df.to_json(filename)
+
+def _build_jobs_df():
+    import glob
+    import os
+    import numpy as np
+    import json
+    from bokeh.plotting import show, output_notebook, figure, ColumnDataSource
+    from bokeh.charts import Scatter
+    from bokeh.models import HoverTool, BoxSelectTool
+    from bokeh.models import BoxZoomTool
+    from lightjob.cli import load_db
+    from lightjob.db import SUCCESS
+    from lightjob.utils import summarize
+
+    db = load_db()
+    rows = []
+    jobs = db.jobs_with(state=SUCCESS)
+    for job in jobs:
+        folder = '../students/jobs/{}'.format(job['summary'])
+        stats = os.path.join(folder, 'stats.csv')
+        if not os.path.exists(stats):
+            continue
+        df_stats = pd.read_csv(stats)
+        df_valid = pd.read_csv(os.path.join(folder, 'valid.csv'))
+        df_valid = df_valid[df_valid.columns[0]].values
+        hypers = job['content']['hypers']
+        start_time = job['life'][0]['dt']
+        end_time = job['life'][0]['dt']    
+        acc = _moving(df_stats['acc'])
+        row = job['content'].copy()
+        del row['hypers']
+        row.update(hypers)
+
+        row['train_acc'] = acc[::1250]
+        row['valid_acc'] = df_valid.tolist()
+        row['max_valid_acc'] = float(df_valid.max())
+        row['last_train_acc'] = row['train_acc'][-1]
+        row['max_train_acc'] = np.max(row['train_acc'])
+        row['id'] = os.path.basename(folder)
+        row['nb_updates'] = len(df_stats)
+        row['start_time'] = start_time
+        row['end_time'] = end_time
+        row['n_epochs'] = (len(df_stats)*32)/40000
+        row['hypers_id'] = summarize(hypers)
+        filename = os.path.join(folder, 'student.th')
+        row['nb_params'] = _get_nb_params_from_filename(filename) 
+        rows.append(row)
+    df = pd.DataFrame(rows)
+    return df
+
+
+def _get_nb_params_from_filename(filename):
+    clf = torch.load(filename, map_location=lambda storage, loc: storage)
+    return _get_nb_params(clf)
+
+
+def _get_nb_params(clf):
+    return sum(map(lambda p:p.view(-1).size(0), clf.parameters()))
+
+
+def _moving(l):
+    v = 0.
+    lm = []
+    for val in l:
+        v = v * 0.9999 + val * 0.0001
+        lm.append(v)
+    return lm
+
+
+def manual():
+    nb_inserted = 0
+    db = load_db()
+    job = db.get_job_by_summary('91e2c05e100ae416e69b7e983c5be906')
+    params = job['content']
+    params['hypers']['fc'] = 1200
+    nb_inserted += db.safe_add_job(params, model=params['model'])
+    params['data_source'] = 'dataset'
+    nb_inserted += db.safe_add_job(params, model=params['model'])
+    params['data_source'] = 'dataset,aux2'
+    nb_inserted += db.safe_add_job(params, model=params['model'])
+
+    job = db.get_job_by_summary('585e3be7a561b047bd2b08ad6bd52e6a')
+    params = job['content']
+    params['data_source'] = 'dataset_simple,aux2'
+    nb_inserted += db.safe_add_job(params, model=params['model'])
+    params['data_source'] = 'dataset_raw,aux2'
+    nb_inserted += db.safe_add_job(params, model=params['model'])
+    print(nb_inserted)
+
 if __name__ == '__main__':
-    result = run(train, insert, clean, train_random, sample, insert_best)
+    result = run(train, insert, clean, train_random, sample, insert_best, resume, export, manual)
